6b1b78a173d3daec2d01868b329da218e117bd6f
Mythrighter <redfoxediting@gmail.com>
2026-02-06 20:21:13 -0500

    Picking up objects!

---
+++ Level Design - Floor is Lava/Assets/PlayerController/PickUp.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    bool canPick;
+    private bool isHolding = false;
+    //private float maxHeldObjects = 1f;
+    private float currentHeldObjects = 0f;
+    [SerializeField] GameObject target;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && canPick == true)
+        {
+            
+            if (isHolding)
+            {
+                DropObject();
+            }
+            else
+            {
+                if (currentHeldObjects == 0f)
+                {
+                    PickUpObject();
+                    Debug.Log("Holding Object");
+                }
+                else
+                {
+                    DropObject();
+                    Debug.Log("Dropped Object");
+                }
+
+            }
+            
+
+        }
+
+    }
+
+    void PickUpObject()
+    {
+        this.transform.parent = target.transform;
+        this.transform.localEulerAngles = new Vector3(10, 10, 10); //this is a bit janky..
+                     //is just as likely to move around. how do I make it consistent?
+        this.GetComponent<Rigidbody>().isKinematic = true;
+        currentHeldObjects = 1;
+    }
+
+    void DropObject()
+    {
+        this.transform.parent = null;
+        this.GetComponent<Rigidbody>().isKinematic = false;
+        currentHeldObjects = 0;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        canPick = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        canPick = false;
+    }
+
+} 
